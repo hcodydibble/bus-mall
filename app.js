cee1c3d2e691f427d581e4a0be38c9c882eeb6f0
Hewlette Dibble <hcodydibble@gmail.com>
2017-08-21 15:09:28 -0700

    made a for loop to fill my array. it was a good time

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var theImages = ['bag.jpg','banana.jpg','bathroom.jpg','boots.jpg','breakfast.jp
   'cthulhu.jpg','dog-duck.jpg','dragon.jpg','pen.jpg','pet-sweep.jpg','scissors.jpg','shark.jpg','sweep.png',
   'tauntaun.jpg','unicorn.jpg','usb.gif','water-can.jpg','wine-glass.jpg'];
 var imgHolder = document.getElementById('imgHolder');
-var leftImg = document.getElementById('img1').src = '';
-var centerImg = document.getElementById('img2').src = '';
-var rightImg = document.getElementById('img3').src = '';
+var leftImg = document.getElementById('img1');
+var centerImg = document.getElementById('img2');
+var rightImg = document.getElementById('img3');
 
-var array = [];
+var productArray = [];
+var currentDisplay = [];
+var previousDisplay = [];
 
 function randomNum(){
   return Math.floor(Math.random() * theImages.length);
@@ -19,4 +21,10 @@ function ProductImage(name,link){
   this.link = link;
   this.clicked = 0;
   this.displayed = 0;
+}
+
+for (var i = 0; i < theImages.length; i++){
+  var imageName = theImages[i];
+  var linkName = 'img/' + imageName;
+  productArray.push(new ProductImage(imageName,linkName));
 }
